fe8f2ea399d5f4964f03b6bd214e8bcfc1ba2bfa
root <root@tony.>
2025-05-17 14:26:09 -0400

    Your commit message

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('datejs'); // step 5
 
+function combineUsers(...arrays) {
+  const combinedObject = {
+    users: [] // 
+  };
 
+  for (let arr of arrays) {
+    if (Array.isArray(arr)) {
+      combinedObject.users.push(...arr); // Merge all arrays
+    }
+  }
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy"); // format for Datejs.
+
+  return combinedObject; 
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
